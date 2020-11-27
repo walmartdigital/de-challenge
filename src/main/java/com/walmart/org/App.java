a73d7c24485fddbf4ebc5be97e0f8f06e5da2bc3
Mariano González <mariano.gonzalez0@walmart.com>
2020-11-27 11:31:39 -0300

    Add MVN Arch

---
+++ src/main/java/com/walmart/org/App.java
@@ -0,0 +1,13 @@
+package com.walmart.org;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
